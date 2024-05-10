9c5c4d57e0b08f2134a0dd5b4b31b08b1f279d9f
Benrise <benrise_x@mail.ru>
2024-05-10 06:50:40 +0300

    Correct and auth only header links

--- src/widgets/header/ui/TheHeader.vue
+++ src/widgets/header/ui/TheHeader.vue
@@ -5,9 +5,24 @@
             <div class="header__content header__content_desktop">
                 <div class="header__left">
                     <div class="header__menu menu">
-                        <ul class="menu__list menu__list_desktop">
+                        <ul v-if="route.name === AppPages.promo" class="menu__list menu__list_desktop">
+                            <router-link class="header__link" :to="appRoutes?.getLogin()">
+                                К поиску
+                            </router-link>
                             <li class="menu__item"><a class="header__link" href="#features">Функции</a></li>
                             <li class="menu__item"><a class="header__link" href="#steps">Шаги</a></li>
+                        </ul>
+                        <ul v-else class="menu__list menu__list_desktop">
+                            <li class="menu__item">
+                                <router-link class="header__link" :to="appRoutes?.getHome()">
+                                    Главная
+                                </router-link>
+                            </li>
+                            <li class="menu__item">
+                                <router-link class="header__link" :to="appRoutes?.getPromo()">
+                                    О сервисе
+                                </router-link>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -30,21 +45,84 @@
                         <div class="sheet__content">
                             <SheetHeader class="sheet__header">Меню</SheetHeader>
                             <div class="header__menu menu">
-                                <ul class="menu__list menu__list_mobile">
+                                <ul v-if="route.name === AppPages.promo" class="menu__list menu__list_mobile">
+                                    <SheetClose>
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link :to="appRoutes?.getHome()" class="header__link">
+                                                К поиску
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
                                     <SheetClose>
                                         <li class="menu__item menu__item_mobile"><a class="header__link" href="#features">Функции</a></li>
                                     </SheetClose> 
                                     <SheetClose>
                                         <li class="menu__item menu__item_mobile"><a class="header__link" href="#steps">Шаги</a></li>
                                     </SheetClose>
+                                    <SheetClose v-if="isAuthorized" >
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link :to="appRoutes?.getProfile()" class="header__link">
+                                                Профиль
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose v-if="isAuthorized" >
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link to="#" class="header__link" variant="secondary" @click="logout">
+                                                Выход
+                                                <IconLogout class="ml-2 w-4 h-4"/>
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose v-if="!isAuthorized" >
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link :to="appRoutes?.getLogin()" class="header__link">
+                                                <IconLogin class="mr-2"/>
+                                                Вход
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                </ul>
+                                <ul v-else class="menu__list menu__list_mobile">
+                                    <SheetClose>
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link class="header__link" :to="appRoutes?.getHome()">
+                                                Главная
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose>
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link class="header__link" :to="appRoutes?.getPromo()">
+                                                О сервисе
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose v-if="isAuthorized">
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link :to="appRoutes?.getProfile()" class="header__link">
+                                                Профиль
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose v-if="isAuthorized">
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link  to="#" class="header__link" variant="secondary" @click="logout">
+                                                Выход
+                                                <IconLogout class="ml-2 w-4 h-4"/>
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
+                                    <SheetClose v-if="!isAuthorized" >
+                                        <li class="menu__item menu__item_mobile">
+                                            <router-link :to="appRoutes?.getLogin()" class="header__link">
+                                                <IconLogin class="mr-2"/>
+                                                Вход
+                                            </router-link>
+                                        </li>
+                                    </SheetClose>
                                 </ul>
                             </div>
-                            <router-link :to="appRoutes?.getLogin()" class="header__link header__link_mobile" >
-                                <Button>
-                                    <IconLogin class="mr-2"/>
-                                    Вход
-                                </Button>
-                            </router-link>
                         </div>
                     </SheetContent>
                   </Sheet>
@@ -60,6 +138,7 @@ import {
   SheetTrigger,
   SheetClose,
   SheetHeader,
+  SheetFooter
 } from '@/shared/ui/sheet';
 import { Button } from '@/shared/ui/button';
 
@@ -69,10 +148,13 @@ import IconLogout from '~icons/heroicons/arrow-right-start-on-rectangle-20-solid
 
 import { UserBadge, UserModel } from '@/entities/user';
 import { AppPages, router, useAppRoutes } from '@/router';
+import { useRoute } from 'vue-router';
 
 const appRoutes = useAppRoutes()
 const userStore = UserModel.useUserStore();
 const isAuthorized = userStore.isAuthorized;
+
+const route = useRoute();
 
 const logout = () => {
     userStore.logout()
