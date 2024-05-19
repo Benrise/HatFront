7f3e96de906fd2feab929ea9b70be528a98be9a9
Benrise <benrise_x@mail.ru>
2024-05-19 17:33:34 +0300

    Minor visual improvements

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,8 @@
 import '@/app/styles/index.scss';
 
-import validators from '@/shared/lib/validators';
 import { createApp } from 'vue';
 import { router } from '@/router';
 import { store } from '@/store';
 import App from './App.vue';
 
-export const application = createApp(App).use(router).use(store).use(validators);
+export const application = createApp(App).use(router).use(store);
